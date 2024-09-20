9c67a4edf2898062e47790182ed89da02c78ba1b
Arvinder Singh <sardarchitect@gmail.com>
2024-09-20 10:36:03 +0530

    Reorganized code directory

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
-export default function Home() {
+"use client"
+import { useThemeContext } from "@/context/theme";import DescriptionFour from "@/components/hero_components/DescriptionFour";
+import DescriptionOne from "@/components/hero_components/DescriptionOne";
+import DescriptionThree from "@/components/hero_components/DescriptionThree";
+import DescriptionTwo from "@/components/hero_components/DescriptionTwo";
+import Hero_one from "@/components/hero_components/HeroSection";
+import ProductReport from "@/components/hero_components/ProductReport";
 
+export default function Home() {
+  const [theme] = useThemeContext();
   return (
-   <main>
-    Hello
-   </main>
+    <div className={`${theme}`}>
+    <div className="text-white ">
+      <Hero_one/>
+      <ProductReport/>
+      <DescriptionOne/>
+      <DescriptionTwo/>
+      <DescriptionThree/>
+      <DescriptionFour/>
+    </div>
+
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,67 +1,77 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-    darkMode: ["class"],
-    content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require("@tailwindcss/typography"),
   ],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  darkMode: ["class"],
   theme: {
-  	extend: {
-  		backgroundImage: {
-  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		}
-  	}
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        }
+      },
+      animation: {
+        fadeIn: 'fadeIn 3s ease-out forwards', // Adding forwards ensures it stays in the final state
+      },
+      colors: {
+        green: {
+          50: '#30AF5B',
+          90: '#292C27',
+        },
+        gray: {
+          10: '#EEEEEE',
+          20: '#A2A2A2',
+          30: '#7B7B7B',
+          50: '#585858',
+          80: '#0D8585',
+          90: '#141414',
+        },
+        orange: {
+          50: '#FF814C',
+        },
+        blue: {
+          70: '#021639',
+        },
+        yellow: {
+          50: '#FEC601',
+        },
+        theme: {blue: 'rgb(2 6 23)'},
+      },
+      backgroundColor: {
+        'rgb-2-6-23': 'rgb(2 6 23)',
+        'theme-color': 'rgb(2 6 23)',
+      },
+      backgroundImage: {
+        'bg_img_blogPage': "url('/left_globe.jpg')",
+        'bg-img-2': "url('/img-2.png')",
+        'feature-bg': "url('/feature-bg.png')",
+        pattern: "url('/pattern.png')",
+        'pattern-2': "url('/pattern-bg.png')",
+        'about-us-header': "url('/about_us/about_us_header.png')",
+      },
+      screens: {
+        xs: '400px',
+        '3xl': '1680px',
+        '4xl': '2200px',
+      },
+      maxWidth: {
+        '10xl': '1512px',
+      },
+      borderRadius: {
+        '5xl': '40px',
+      },
+      fontFamily: {
+        Merriweather : ['Merriweather'],
+        playfair_display : ['var(--font-playfair_display)'],
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate")],
 };
-export default config;
